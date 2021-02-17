447033b9c63c7fe8300a74aff2627a17e6523961
Andrei <andreifrolov83@gmail.com>
2021-02-17 09:08:38 +0300

    init: start typical-arrays-problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
+function isValidArray (array) {
+    if (typeof array === "undefined") return false;
+    else if (array.length === 0) return false;
+    else return true;
+}
 
 exports.min = function min (array) {
-  return 0;
+  if (!isValidArray(array)) return 0;
+  let min = array[0]
+  for (let index = 1; index < array.length; index++) {
+      if (array[index] < min) min = array[index];
+  }
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!isValidArray(array)) return 0;
+    let max = array[0]
+    for (let index = 1; index < array.length; index++) {
+        if (array[index] > max) max = array[index];
+    }
+    return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (!isValidArray(array)) return 0;
+    let sum = 0;
+    array.forEach( (item) => {sum += item;});
+    return sum/array.length;
 }
